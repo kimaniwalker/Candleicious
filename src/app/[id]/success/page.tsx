56daa5faa6a2b71048751318c439d7d6ae133560
Kimani Walker <kimani.walker@shipt.com>
2023-06-03 15:06:52 -0500

    [feat]-fix success page

--- src/app/[id]/success/page.tsx
+++ src/app/[id]/success/page.tsx
@@ -30,13 +30,13 @@ export default function SuccessPage() {
                     Thank you so much for supporting Candleicious! We truly appreciate your business and trust in our handmade candles to bring warmth and serenity to your space.
                 </Message>
                 <Message>
-                    Your order means a lot to us, and we are committed to ensuring your complete satisfaction. If you have any questions or concerns, please don't hesitate to reach out to us via email at <b>admin@kustomcharmz.com</b>. Our dedicated team is here to assist you and address any inquiries you may have.
+                    Your order means a lot to us, and we are committed to ensuring your complete satisfaction. If you have any questions or concerns, please don`t hesitate to reach out to us via email at <b>admin@kustomcharmz.com</b>. Our dedicated team is here to assist you and address any inquiries you may have.
                 </Message>
                 <Message>
                     Rest assured that we are working diligently to fulfill your order as soon as possible. Due to the meticulous craftsmanship that goes into each candle, it may take up to 2 weeks to complete and ship your order. We kindly ask for your patience during this time, as we strive to maintain the highest quality standards for every Candleicious product.
                 </Message>
                 <Message>
-                    We also want to extend an invitation for you to become a VIP member of Candleicious. By joining our VIP membership program, you'll gain access to exclusive deals, discounts, and special promotions. As a VIP member, you'll be the first to know about our new releases and receive additional perks as a token of our appreciation for your continued support.
+                    We also want to extend an invitation for you to become a VIP member of Candleicious. By joining our VIP membership program, you`ll gain access to exclusive deals, discounts, and special promotions. As a VIP member, you`ll be the first to know about our new releases and receive additional perks as a token of our appreciation for your continued support.
                 </Message>
                 <Message>
                     Once again, thank you for choosing Candleicious. Your loyalty and trust mean the world to us. We are committed to creating exceptional candles that enhance your ambiance and bring joy to your life. We look forward to serving you and brightening your moments.
